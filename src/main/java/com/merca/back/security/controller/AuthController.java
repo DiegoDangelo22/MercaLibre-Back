8bf0148f633fe95b3031bbcfda59827c70ff1b8b
Kaxzark <dangelo.diego.gabriel@gmail.com>
2023-07-12 19:23:41 -0300

    .

--- src/main/java/com/merca/back/security/controller/AuthController.java
+++ src/main/java/com/merca/back/security/controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200","https://mercalibre-365b2.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
